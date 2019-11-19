6cdc110f2cf8b0322b710fe35e97e72666d05cf7
Marco Mollinetti <marco.mollinetti@gmail.com>
2019-11-19 21:22:11 +0900

    finished class 4_5

--- Scripts/Class 4_5.R
+++ Scripts/Class 4_5.R
@@ -1,6 +1,6 @@
 #BEFORE STARTING OUR CLASS, PLEASE RUN THIS SNIPPET TO INSTALL/LOAD OUR LIBRARIES NEEDED FOR THIS LESSON
 
-packages_needed <- c('car', 'reshape2', 'psych', 'multcomp', 'effects')
+packages_needed <- c('car', 'reshape2', 'psych', 'multcomp', 'effects', 'pwr', 'broom')
 for (package_name in packages_needed) {      
   if (!(package_name %in% rownames(installed.packages()))){
     install.packages(package_name)
@@ -17,29 +17,30 @@ for (i in 1:length(packages_needed))
 ###################################################################################################
 #ANCOVA
 
-lepr <- read.table('leprosy.dat', header = T)
+lepr <- read.csv('leprosy.csv', header = T)
 attach(lepr)
 
 str(lepr)
 
 #equality of slopes interaction
-fitted.lepr <- aov(x ~ drug + y + drug:y, data = lepr)
+fitted.lepr <- aov(After ~ Drug * Before, data = lepr)
+
 #we are interested in the interaction term
 summary(fitted.lepr)
 
 #linearity of slopes (Eyeballing, just like Rick's neutrino bomb)
-plot(y, x, col = drug, xlab = "X", ylab = "Y")
+plot(After, Before, col = Drug, xlab = "Before", ylab = "After")
 
 #Equality of the groups on the covariate
 #Anova with the groups
-fitted.eq<- aov(y ~ drug, data = lepr)
-summary(fitted.eq)
+fitted.eq<- aov(Before ~ Drug, data = lepr)
 
 #Homogeneity of variances - levene test
-leveneTest(x ~ drug, center = mean, data = lepr)
+leveneTest(After ~ Drug, center = mean, data = lepr)
 
 #RUNNING THE ANCOVA (ORDER OF THE LINEAR FIT MATTERS!)
-fit.ancova <- aov(x ~ y + drug, data = lepr)  # NOTE: covariate goes first!! there is no interaction
+fit.ancova <- aov(After ~ Before + Drug, data = lepr)  # NOTE: covariate goes first!! there is no interaction
+
 summary(fit.ancova)
 
 #CHECK FOR NORMALITY
@@ -47,12 +48,16 @@ summary(fit.ancova)
 residuals <- resid(fit.ancova)
 shapiro.test(residuals)
 
+#diagnostic partial plot
+lep.drug <- aov(After ~ Drug, lepr)
+summary(lep.drug)
+
 #histogram for the residuals
 hist(residuals)
 
 # to get the ’real’ p value and difference,
 # uses the general linear hypothesis function
-posthoc <- glht(fit.ancova, linfct = mcp(drug = "Tukey"))
+posthoc <- glht(fit.ancova, linfct = mcp(Drug = "Tukey"))
 
 summary(posthoc)
 
@@ -63,9 +68,28 @@ describeBy(x, drug)
 
 #from the effects library
 #adjusted group means
-effect("drug", fit.ancova)
+effect("Drug", fit.ancova)
+
+LM <- lm(After ~ Before + Drug, data = lepr)
+anova(LM)
+
+summary(LM)
+
+#checking the r-2
+tidy_aov <- tidy(fit.ancova)
+
+#calculate the r-squared
+sum_squares_regression <- tidy_aov$sumsq[1]
+sum_squares_residuals <- tidy_aov$sumsq[2]
+
+R_squared <- sum_squares_regression /(sum_squares_regression + sum_squares_residuals)
 
 
+#power analysis of the ancova
+pwr.f2.test(u = 2, f2 = 0.6389/ (1-0.6389), sig.level = 0.05, power = 0.85)
+
+#n = v + u + 1
+pwr.f2.test(u = 2, v = 7, f2 = 0.6389/ (1-0.6389), sig.level = 0.05)
 ###################################################################################################
 ###################################################################################################
 
@@ -100,6 +124,7 @@ colnames(un.sprays) <- c('A','B','C','D','E','F')
 
 #any extra plot
 
+#Power Analysis!
 
 
 
